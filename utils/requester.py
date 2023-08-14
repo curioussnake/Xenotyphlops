66493b781df950736f98f2dfda3b2cf00d162981
sneakysnake <p.e.adamowicz@gmail.com>
2023-08-14 17:43:37 +0200

    Add headers parameter in requester.py response and set default header

--- utils/requester.py
+++ utils/requester.py
@@ -6,6 +6,11 @@ import requests
 
 class Requester:
     def requester(self, method, url, path="", body="", **kwargs):
+        headers = kwargs.get("headers", {})
+
+        if "content-type" not in headers:
+            headers["content-type"] = "application/json"
+
         if "headers" in kwargs.keys() and "content-type" in kwargs["headers"].keys():
             data = json.dumps(body)
         else:
@@ -15,7 +20,7 @@ class Requester:
             method=method,
             url=f"{url}{'/' if path != '' else ''}{path}",
             data=data,
-            headers=kwargs.get("headers", {})
+            headers=headers
         )
         logging.info(f"response.status_code: {response.status_code}")
         logging.info(f"response.headers: {response.headers}")
