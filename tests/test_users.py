b48dcbe162be64c42f57845b705f4db273823c1a
sneakysnake <p.e.adamowicz@gmail.com>
2023-09-11 08:52:50 +0200

    change response codes in assertions.

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,30 @@
 def test_get_all_users(users_api):
     all_users = users_api.get_list_of_all_users()
-    # print("all users result: " + all_users.json())
+    print("all users result: " + all_users.text)
     assert len(all_users.json()) == 2
 
 
 def test_create_user(users_api):
-    create_user = users_api.create_user("Pablo", "Pueblo", role="MODERATOR")
-    print("user created: " + create_user.text)
+    all_users = users_api.get_list_of_all_users()
+    create_user = users_api.create_user("Pedro", "Sanchez", role="MODERATOR")
+    assert create_user.status_code == 201
+    new_user = users_api.get_user_by_id(create_user.json()["id"])
+    # print(new_user.json()["name"])
+    assert new_user.status_code == 200
+    new_user_json = new_user.json()
+    assert new_user_json["name"] == "Pablo", "Incorrect name!"
+    assert new_user_json["surname"] == "Pueblo", "Incorrect surname!"
+    assert new_user_json["role"] == "MODERATOR", "Incorrect role!" #To beda miekkie asercje
+    all_users_after_creation = users_api.get_list_of_all_users()
+    assert all_users_after_creation.status_code == 200
+    assert len(all_users.json()) < len(all_users_after_creation.json()) #To bedzie miekka asercja
+
 
 
 def test_get_user_by_id_positive(users_api):
     user_id = "tfirsttest"
     get_user_by_id = users_api.get_user_by_id(user_id)
-    print("user with id =" + user_id + " is: " + get_user_by_id.content)
+    print("user with id =" + user_id + " is: " + get_user_by_id.text)
 
 
 def test_update_user(users_api):
@@ -29,9 +41,6 @@ def test_update_user(users_api):
     #Metoda Except - zamiast asercji - assert expectations - zamiast zwyklych asercji w ktorych niepowodzenie dowolnej z nich powoduje przerwanie sprawdzania kolejnych
     # W przypadku assert expectation - punkt przerwania - jezeli ktoras z metod expect nie zostanie spelniona.
 
-    #TODO: 1. Przebudowac metody w usersapi i rolesapi, zeby przyjmowaly wartosci domyslne headerow,
     #TODO: 2. test_users, test_roles - dodac asercje
     #TODO: 3. Ogarnac Expect assert (Biblioteka sie nazywa Delayed Assert
     #TODO: 4. Sprawdzic Runtime environment, zeby byl ustawiony w rootcie (A nie w katalogu /tests/ poniewaz w /tests/ nie ma fixtureow wiec nie moze z nich skorzystac!
-
-
