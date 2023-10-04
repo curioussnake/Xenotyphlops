cf6eb1c4c3882374cb2026bf0a009a7aa215cad4
sneakysnake <p.e.adamowicz@gmail.com>
2023-10-04 21:54:53 +0200

    Use expect instead of assert where it's needed

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -1,7 +1,10 @@
+from delayed_assert import expect
+
+
 def test_get_list_of_all_roles(roles_api):
     get_list = roles_api.get_list_of_all_roles()
     assert get_list.status_code == 200
-    assert len(get_list.json()) == 4
+    expect(len(get_list.json()) == 4, "Number of roles is incorrect!")
 
 
 def test_get_role_by_id(roles_api):
@@ -9,7 +12,7 @@ def test_get_role_by_id(roles_api):
     get_role = roles_api.get_role_by_id(role_id, accept="application/json")
     assert get_role.status_code == 200
     get_role_by_id_json = get_role.json()
-    assert get_role_by_id_json["name"] == "Admin", "Incorrect id!"
-    assert get_role_by_id_json["description"] == "For admit content and service.", "Incorrect description"
+    expect(get_role_by_id_json["name"] == "Admin", "Incorrect id!")
+    expect(get_role_by_id_json["description"] == "For admit content and service.", "Incorrect description")
     print("role with id: " + role_id + " is " + get_role.text)
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,11 @@
+from delayed_assert import expect
+
+
 def test_get_all_users(users_api):
     all_users = users_api.get_list_of_all_users()
     print("all users result: " + all_users.text)
     assert all_users.status_code == 200
-    assert len(all_users.json()) == 2
+    expect(len(all_users.json()) == 2, "Number of accounts is incorrect!")
 
 
 def test_create_user(users_api):
@@ -13,13 +16,12 @@ def test_create_user(users_api):
     # print(new_user.json()["name"])
     assert new_user.status_code == 200
     new_user_json = new_user.json()
-    assert new_user_json["name"] == "Pablo", "Incorrect name!"
-    assert new_user_json["surname"] == "Pueblo", "Incorrect surname!"
-    assert new_user_json["role"] == "MODERATOR", "Incorrect role!" #To beda miekkie asercje
+    expect(new_user_json["name"] == "Pablo", "Incorrect name!")
+    expect(new_user_json["surname"] == "Pueblo", "Incorrect surname!")
+    expect(new_user_json["role"] == "MODERATOR", "Incorrect role!") #To beda miekkie asercje
     all_users_after_creation = users_api.get_list_of_all_users()
     assert all_users_after_creation.status_code == 200
     assert len(all_users.json()) < len(all_users_after_creation.json()) #To bedzie miekka asercja
-
 
 
 def test_get_user_by_id_positive(users_api):
@@ -29,9 +31,9 @@ def test_get_user_by_id_positive(users_api):
     get_user_by_id_json = get_user_by_id.json()
     # print("here is my printed stuff")
     # print(get_user_by_id_json)
-    assert get_user_by_id_json["name"] == "Test"
-    assert get_user_by_id_json["lastname"] == "FirstTest"
-    assert get_user_by_id_json["role"] == "NONE"
+    expect(get_user_by_id_json["name"] == "Test")
+    expect(get_user_by_id_json["lastname"] == "FirstTest")
+    expect(get_user_by_id_json["role"] == "NONE")
 
 
 def test_update_user(users_api):
@@ -40,7 +42,7 @@ def test_update_user(users_api):
     update_user = users_api.update_user("tfirsttest", "Pablo", "Pueblo", "MODERATOR")
     print("user with id =" + user_id + " and data " + get_user_by_id.content + " updated with: " + update_user.content)
 
-    #Delete assert (miekka asercja)
+    #Delayed assert (miekka asercja)
     #Zweryfikowac w asercji: 1.Kod odpowiedzi, 2.Dane, 3.Jezeli jest
     # POST lub UPDATE to zweryfikowac czy dane sa poprawnie zaaktualizowane
 
@@ -48,6 +50,4 @@ def test_update_user(users_api):
     #Metoda Except - zamiast asercji - assert expectations - zamiast zwyklych asercji w ktorych niepowodzenie dowolnej z nich powoduje przerwanie sprawdzania kolejnych
     # W przypadku assert expectation - punkt przerwania - jezeli ktoras z metod expect nie zostanie spelniona.
 
-    #TODO: 2. test_users, test_roles - dodac asercje
-    #TODO: 3. Ogarnac Expect assert (Biblioteka sie nazywa Delayed Assert
     #TODO: 4. Sprawdzic Runtime environment, zeby byl ustawiony w rootcie (A nie w katalogu /tests/ poniewaz w /tests/ nie ma fixtureow wiec nie moze z nich skorzystac!
